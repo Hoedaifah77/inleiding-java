1676ac87530aae38214d3b6e9e08dbeb2277728e
Hoedaifah77 <2087702@talnet.nl>
2018-09-04 13:14:38 +0200

    name finished

--- src/h01/HalloRocDev.java
+++ src/h01/HalloRocDev.java
@@ -1,11 +1,20 @@
 package h01;
 
-/**
- * Created by piet on 31-08-16.
- */
-public class HalloRocDev {
+import java.awt.*;
+import java.applet.*;
 
-    public static void main(String[] args) {
-        System.out.println("Hallo Piet!");
+public class HalloRocDev extends Applet {
+
+    public void init() {
+        setBackground(Color.blue);
+    }
+
+    public void paint(Graphics g) {
+        g.setColor(Color.yellow);
+        g.drawString("Hoedaifah!!", 50, 60 );
+        g.setColor(Color.yellow);
+        g.drawString("elfathi!!", 50, 80 );
     }
 }
+
+
