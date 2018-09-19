947c963a55e7f1851037556104b2da4006d6a04a
Hoedaifah77 <2087702@talnet.nl>
2018-09-19 13:10:39 +0200

    Added comments

--- src/h04/Opdracht44.java
+++ src/h04/Opdracht44.java
@@ -6,7 +6,6 @@ public class Opdracht44 extends Applet {
     }
 
     public Opdracht44() throws HeadlessException {
-        // HALLO
     }
 
     public void paint(Graphics g) {
--- src/h06/Opdracht63.java
+++ src/h06/Opdracht63.java
@@ -7,6 +7,14 @@ import java.awt.*;
 
 public class Opdracht63 extends Applet {
 
+    int a;
+
+    int b;
+
+    int c;
+
+    int d;
+
 
 
 
@@ -14,11 +22,28 @@ public class Opdracht63 extends Applet {
 
     public void init() {
 
+        a = 2147483647;
+
+        b = 1;
+
+        c = a + b;
+
+        d = a - b;
+
+
+
 
     }
 
 
     public void paint(Graphics g) {
+
+        g.setColor(Color.red);
+        g.drawString("negatief   " + c, 20,20);
+
+
+        g.setColor(Color.blue);
+        g.drawString("positief   " + d ,19,50);
 
 
 
