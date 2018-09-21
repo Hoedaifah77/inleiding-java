3016730c200b62e8a47cb2e598e00dc047723ec4
Hoedaifah77 <2087702@talnet.nl>
2018-09-21 12:52:05 +0200

    added contentttt

---
+++ src/h08/Opdracht81.java
@@ -0,0 +1,64 @@
+package h08;
+
+import java.awt.*;
+import java.applet.*;
+import java.awt.event.*;
+
+
+public class Opdracht81 extends Applet {
+    Button okKnop;
+    Button resetKnop;
+
+    String input;
+
+    TextField tekstvak;
+
+    public void init() {
+        tekstvak = new TextField("", 20);
+        tekstvak.addActionListener(new tekstvakListener());
+        add(tekstvak);
+
+        okKnop = new Button("Ok");
+        okKnop.addActionListener(new okKnopListener ());
+        add(okKnop);
+
+        resetKnop = new Button("Reset");
+        resetKnop.addActionListener(new resetKnopListener ());
+        add(resetKnop);
+
+        input = " ";
+
+
+    }
+
+    public void paint(Graphics g) {
+
+        g.drawString("" + input, 40,40);
+
+    }
+
+    class okKnopListener implements ActionListener {
+        public void actionPerformed( ActionEvent e ) {
+            input = tekstvak.getText();
+            repaint();
+
+        }
+
+    }
+    class resetKnopListener implements ActionListener {
+        public void actionPerformed( ActionEvent e ) {
+            tekstvak.setText("");
+            repaint();
+
+        }
+
+    }
+    class tekstvakListener implements ActionListener {
+        public void actionPerformed( ActionEvent e ) {
+            input = tekstvak.getText();
+            repaint();
+
+        }
+
+    }
+}
